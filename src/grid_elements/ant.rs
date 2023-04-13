3ee10b320fef89198a1989ad68fa1c7689f853b2
bagel897 <bageljr897@protonmail.com>
2023-04-12 21:14:31 -0500

    init state machine

--- src/grid_elements/ant.rs
+++ src/grid_elements/ant.rs
@@ -7,38 +7,69 @@ use crate::{
 
 use super::grid_element::GridElement;
 #[derive(Debug)]
+enum State {
+    Wandering,
+    Food,
+    Battle,
+    Carrying,
+}
+#[derive(Debug)]
 pub(crate) struct Ant {
     dir: Dir,
     pos: Coord,
+    state: State,
 }
 impl GridElement for Ant {
     fn new(pos: &Coord) -> Self {
         return Ant {
             dir: Dir::UP,
             pos: pos.clone(),
+            state: State::Wandering,
         };
     }
     fn exists(&self) -> bool {
         return true;
     }
+
     fn decide(&mut self, grid: &AntGrid) -> Coord {
-        let next = self.next();
-        if next.is_none() || grid.is_blocked(&next.unwrap()) {
-            self.dir = self.dir.turn();
-            return self.pos;
-        } else {
-            self.pos = next.unwrap();
-            return next.unwrap();
+        match self.state {
+            State::Wandering => self.wander(grid),
+            State::Food => todo!(),
+            State::Battle => todo!(),
+            State::Carrying => todo!(),
         }
     }
 }
 impl Display for Ant {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "x")
+        let state = match self.state {
+            State::Wandering => "w",
+            State::Carrying => "c",
+            State::Food => "s",
+            State::Battle => "b",
+        };
+        write!(f, "{}", state)
     }
 }
 impl Ant {
     pub(self) fn next(&self) -> Option<Coord> {
         return self.pos.next_cell(&self.dir);
+    }
+
+    fn wander(&mut self, grid: &AntGrid) -> Coord {
+        match self.next() {
+            None => {
+                self.dir = self.dir.turn();
+                return self.pos;
+            }
+            Some(i) => {
+                if grid.is_blocked(&i) {
+                    self.dir = self.dir.turn();
+                    return self.pos;
+                }
+                self.pos = i;
+                return i;
+            }
+        }
     }
 }
