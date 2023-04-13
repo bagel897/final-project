a08423b54a8ddb6cf04bf940887a4e5b5328b636
bagel897 <bageljr897@protonmail.com>
2023-04-12 23:37:19 -0500

    oops add the food item

---
+++ src/grid_elements/food.rs
@@ -0,0 +1,29 @@
+use std::fmt::Display;
+
+use crate::coord::Coord;
+
+use super::grid_element::GridElement;
+
+#[derive(Debug)]
+pub(crate) struct Food {
+    pub pos: Coord,
+}
+impl GridElement for Food {
+    fn exists(&self) -> bool {
+        return true;
+    }
+    fn new(pos: &Coord) -> Self
+    where
+        Self: Sized,
+    {
+        return Food { pos: pos.clone() };
+    }
+    fn decide(&mut self, grid: &crate::ant_grid::AntGrid) -> Coord {
+        return self.pos;
+    }
+}
+impl Display for Food {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "f")
+    }
+}
