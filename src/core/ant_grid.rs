4bb96cc41b3e7316d144ed475efb751d9e8a5cde
bagel897 <bageljr897@protonmail.com>
2023-04-21 13:04:19 -0500

    refactor: split antgrid

--- src/core/ant_grid.rs
+++ src/core/ant_grid.rs
@@ -25,7 +25,6 @@ impl Default for Options {
     }
 }
 use super::{
-    grid::GridIterator,
     grid_elements::food::FOOD_ELEMENT,
     signals::Signal,
     team_element::{ElementType, TeamElement},
@@ -37,16 +36,7 @@ pub(crate) struct AntGrid {
     rng: rand::rngs::ThreadRng,
 }
 impl AntGrid {
-    pub fn new(rows: usize, cols: usize) -> Self {
-        AntGrid {
-            grid: Grid::new(rows, cols),
-            elements: MultiMap::new(),
-            rng: thread_rng(),
-            options: Options::default(),
-        }
-    }
-
-    pub fn is_blocked(&self, coord: &Coord) -> bool {
+    pub(super) fn is_blocked(&self, coord: &Coord) -> bool {
         if !self.grid.does_exist(coord) {
             return true;
         }
@@ -54,47 +44,6 @@ impl AntGrid {
         return get.elem.clone().map_or(false, |g| {
             return g.borrow().exists();
         });
-    }
-    fn run_decide(&mut self) {
-        let mut to_iter: VecDeque<Rc<RefCell<dyn GridElement>>> = self
-            .elements
-            .flat_iter_mut()
-            .map(|(_, v)| v.to_owned())
-            .collect();
-        while !to_iter.is_empty() {
-            let ant = to_iter.pop_front().unwrap();
-            let old_pos = ant.borrow().pos().clone();
-            let c = ant.borrow_mut().decide(self);
-            self.grid.get_mut(&old_pos).elem = None;
-            if c.is_none() {
-                continue;
-            }
-            self.grid.get_mut(&c.unwrap()).elem = Some(ant.clone());
-        }
-    }
-    fn adjust(&mut self, distance: f64) -> f64 {
-        let top = 1.0 + self.options.smell;
-        let bot = 1.0 - self.options.smell;
-        let rand = self.rng.sample(Uniform::new(bot, top));
-        return rand * distance;
-    }
-    fn distance(&mut self, team_elem: &TeamElement, pt: &Coord) -> Option<f64> {
-        if self.is_blocked(pt) {
-            return None;
-        }
-        return Some(
-            self.adjust(
-                self.elements
-                    .get_vec(team_elem)?
-                    .iter()
-                    .filter_map(|f| f.try_borrow().ok())
-                    .map(|f| pt.distance(&f.pos()))
-                    .min_by(|x, y| x.total_cmp(y))?,
-            ),
-        );
-    }
-    pub fn distance_to_food(&mut self, pt: &Coord) -> Option<f64> {
-        return self.distance(&FOOD_ELEMENT, pt);
     }
     pub(super) fn send_signal(&mut self, pt: &Coord, signal: Signal, team_elem: TeamElement) {
         for mut i in self
@@ -123,7 +72,51 @@ impl AntGrid {
             }
         }
     }
-    pub fn distance_to_enemy(&mut self, pt: &Coord, team: &Team) -> Option<f64> {
+    pub(super) fn attack(&self, coord: &Coord, team: &Team) {
+        assert!(self.is_enemy(coord, team));
+        let ant = self.grid.get(coord).elem.clone().unwrap();
+        let mut other_entity = ant.borrow_mut();
+        other_entity.attacked(1);
+    }
+    pub(super) fn is_enemy(&self, coord: &Coord, team: &Team) -> bool {
+        if !self.grid.does_exist(coord) {
+            return false;
+        }
+        let ant = &self.grid.get(coord).elem;
+
+        let other_team = match ant {
+            None => None,
+            Some(a) => a.borrow().team(),
+        };
+        return match other_team {
+            None => false,
+            Some(t) => &t != team,
+        };
+    }
+    pub(super) fn is_food(&self, coord: &Coord) -> bool {
+        if !self.grid.does_exist(coord) {
+            return false;
+        }
+        let ant = self.grid.get(coord).get_elem(&coord);
+
+        return ant.borrow().type_elem() == ElementType::Food;
+    }
+    pub(super) fn is_hive_same_team(&self, coord: &Coord, team: Team) -> bool {
+        if !self.grid.does_exist(coord) {
+            return false;
+        }
+        let ant = self.grid.get(coord).get_elem(&coord);
+        let elem = ant.borrow().team_element();
+        return elem
+            == TeamElement {
+                element: ElementType::Hive,
+                team: Some(team),
+            };
+    }
+    pub(super) fn distance_to_food(&mut self, pt: &Coord) -> Option<f64> {
+        return self.distance(&FOOD_ELEMENT, pt);
+    }
+    pub(super) fn distance_to_enemy(&mut self, pt: &Coord, team: &Team) -> Option<f64> {
         if self.is_blocked(pt) {
             return None;
         }
@@ -140,7 +133,7 @@ impl AntGrid {
             .filter_map(|f| f)
             .min_by(|x, y| x.total_cmp(y))
     }
-    pub fn distance_to_hive(&mut self, pt: &Coord, team: &Team) -> Option<f64> {
+    pub(super) fn distance_to_hive(&mut self, pt: &Coord, team: &Team) -> Option<f64> {
         if self.is_blocked(pt) {
             return None;
         }
@@ -152,51 +145,38 @@ impl AntGrid {
             pt,
         );
     }
+}
+impl AntGrid {
+    pub fn new(rows: usize, cols: usize) -> Self {
+        AntGrid {
+            grid: Grid::new(rows, cols),
+            elements: MultiMap::new(),
+            rng: thread_rng(),
+            options: Options::default(),
+        }
+    }
+
+    fn run_decide(&mut self) {
+        let mut to_iter: VecDeque<Rc<RefCell<dyn GridElement>>> = self
+            .elements
+            .flat_iter_mut()
+            .map(|(_, v)| v.to_owned())
+            .collect();
+        while !to_iter.is_empty() {
+            let ant = to_iter.pop_front().unwrap();
+            let old_pos = ant.borrow().pos().clone();
+            let c = ant.borrow_mut().decide(self);
+            self.grid.get_mut(&old_pos).elem = None;
+            if c.is_none() {
+                continue;
+            }
+            self.grid.get_mut(&c.unwrap()).elem = Some(ant.clone());
+        }
+    }
     pub fn run_round(&mut self) {
         self.run_decide();
     }
 
-    pub fn attack(&self, coord: &Coord, team: &Team) {
-        assert!(self.is_enemy(coord, team));
-        let ant = self.grid.get(coord).elem.clone().unwrap();
-        let mut other_entity = ant.borrow_mut();
-        other_entity.attacked(1);
-    }
-    pub fn is_enemy(&self, coord: &Coord, team: &Team) -> bool {
-        if !self.grid.does_exist(coord) {
-            return false;
-        }
-        let ant = &self.grid.get(coord).elem;
-
-        let other_team = match ant {
-            None => None,
-            Some(a) => a.borrow().team(),
-        };
-        return match other_team {
-            None => false,
-            Some(t) => &t != team,
-        };
-    }
-    pub fn is_food(&self, coord: &Coord) -> bool {
-        if !self.grid.does_exist(coord) {
-            return false;
-        }
-        let ant = self.grid.get(coord).get_elem(&coord);
-
-        return ant.borrow().type_elem() == ElementType::Food;
-    }
-    pub fn is_hive_same_team(&self, coord: &Coord, team: Team) -> bool {
-        if !self.grid.does_exist(coord) {
-            return false;
-        }
-        let ant = self.grid.get(coord).get_elem(&coord);
-        let elem = ant.borrow().team_element();
-        return elem
-            == TeamElement {
-                element: ElementType::Hive,
-                team: Some(team),
-            };
-    }
     pub fn put<T: GridElement + 'static>(&mut self, elem: T) {
         let pos = elem.pos().clone();
         let elem_ref = Rc::new(RefCell::new(elem));
@@ -219,12 +199,31 @@ impl AntGrid {
     pub fn cols(&self) -> usize {
         return self.grid.cols;
     }
-
-    pub fn iter(&self) -> GridIterator {
-        return self.grid.iter();
-    }
     pub fn export(&self) -> Grid {
         return self.grid.clone();
+    }
+}
+impl AntGrid {
+    fn adjust(&mut self, distance: f64) -> f64 {
+        let top = 1.0 + self.options.smell;
+        let bot = 1.0 - self.options.smell;
+        let rand = self.rng.sample(Uniform::new(bot, top));
+        return rand * distance;
+    }
+    fn distance(&mut self, team_elem: &TeamElement, pt: &Coord) -> Option<f64> {
+        if self.is_blocked(pt) {
+            return None;
+        }
+        return Some(
+            self.adjust(
+                self.elements
+                    .get_vec(team_elem)?
+                    .iter()
+                    .filter_map(|f| f.try_borrow().ok())
+                    .map(|f| pt.distance(&f.pos()))
+                    .min_by(|x, y| x.total_cmp(y))?,
+            ),
+        );
     }
 }
 impl Display for AntGrid {
